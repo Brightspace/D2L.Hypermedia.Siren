5f96eb90560e1e5c38cbcd4325fa7417f36d8501
Ryan Mahler <Ryan.Mahler@D2L.com>
2016-12-19 13:51:59 -0500

    Adding extension methods and tests
    These allow for a bit more fluent of an interface when dealing with the various ISiren* objects. For example, one can do ISirenEntity.TryGetLinkByRel("self", out link) if you want to get the self link of an entity. To skip the Try-ness, could also just do ISirenEntity.LinksByRel("self")[0].

---
+++ D2L.Hypermedia.Siren/SirenActionExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace D2L.Hypermedia.Siren {
+
+	public static class SirenActionExtensions {
+
+		public static bool TryGetFieldByName(
+				this ISirenAction @this,
+				string name,
+				out ISirenField field
+		) {
+			field = @this.Fields.FirstOrDefault( f => f.Name.Equals( name ) );
+			return field != default( ISirenField );
+		}
+
+		public static bool TryGetFieldByClass(
+				this ISirenAction @this,
+				string @class,
+				out ISirenField field
+		) {
+			field = @this.FieldsByClass( @class ).FirstOrDefault();
+			return field != default( ISirenField );
+		}
+
+		public static bool TryGetFieldByType(
+				this ISirenAction @this,
+				string type,
+				out ISirenField field
+		) {
+			field = @this.FieldsByType( type ).FirstOrDefault();
+			return field != default( ISirenField );
+		}
+
+		public static IEnumerable<ISirenField> FieldsByClass(
+				this ISirenAction @this,
+				string @class
+		) {
+			return @this.Fields.Where( field => field.Class.Contains( @class ) );
+		}
+
+		public static IEnumerable<ISirenField> FieldsByType(
+				this ISirenAction @this,
+				string type
+		) {
+			return @this.Fields.Where( field => field.Type.Equals( type ) );
+		}
+
+	}
+
+}
---
+++ D2L.Hypermedia.Siren/SirenEntityExtensions.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace D2L.Hypermedia.Siren {
+
+	public static class SirenEntityExtensions {
+
+		public static bool TryGetActionByName(
+				this ISirenEntity @this,
+				string name,
+				out ISirenAction action
+		) {
+			action = @this.Actions.FirstOrDefault( a => a.Name.Equals( name ) );
+			return action != default( ISirenAction );
+		}
+
+		public static bool TryGetActionByClass(
+				this ISirenEntity @this,
+				string @class,
+				out ISirenAction action
+		) {
+			action = @this.ActionsByClass( @class ).FirstOrDefault();
+			return action != default( ISirenAction );
+		}
+
+		public static bool TryGetLinkByRel(
+				this ISirenEntity @this,
+				string rel,
+				out ISirenLink link
+		) {
+			link = @this.LinksByRel( rel ).FirstOrDefault();
+			return link != default( ISirenLink );
+		}
+
+		public static bool TryGetLinkByClass(
+				this ISirenEntity @this,
+				string @class,
+				out ISirenLink link
+		) {
+			link = @this.LinksByClass( @class ).FirstOrDefault();
+			return link != default( ISirenLink );
+		}
+
+		public static bool TryGetLinkByType(
+				this ISirenEntity @this,
+				string type,
+				out ISirenLink link
+		) {
+			link = @this.LinksByType( type ).FirstOrDefault();
+			return link != default( ISirenLink );
+		}
+
+		public static bool TryGetSubEntityByRel(
+				this ISirenEntity @this,
+				string rel,
+				out ISirenEntity entity
+		) {
+			entity = @this.SubEntitiesByRel( rel ).FirstOrDefault();
+			return entity != default( ISirenEntity );
+		}
+
+		public static bool TryGetSubEntityByClass(
+				this ISirenEntity @this,
+				string @class,
+				out ISirenEntity entity
+		) {
+			entity = @this.SubEntitiesByClass( @class ).FirstOrDefault();
+			return entity != default( ISirenEntity );
+		}
+
+		public static bool TryGetSubEntityByType(
+				this ISirenEntity @this,
+				string type,
+				out ISirenEntity entity
+		) {
+			entity = @this.SubEntitiesByType( type ).FirstOrDefault();
+			return entity != default( ISirenEntity );
+		}
+
+		public static IEnumerable<ISirenAction> ActionsByClass(
+				this ISirenEntity @this,
+				string @class
+		) {
+			return @this.Actions.Where( action => action.Class.Contains( @class ) );
+		}
+
+		public static IEnumerable<ISirenLink> LinksByRel(
+				this ISirenEntity @this,
+				string rel
+		) {
+			return @this.Links.Where( link => link.Rel.Contains( rel ) );
+		}
+
+		public static IEnumerable<ISirenLink> LinksByClass(
+				this ISirenEntity @this,
+				string @class
+		) {
+			return @this.Links.Where( link => link.Class.Contains( @class ) );
+		}
+
+		public static IEnumerable<ISirenLink> LinksByType(
+				this ISirenEntity @this,
+				string type
+		) {
+			return @this.Links.Where( link => link.Type.Equals( type ) );
+		}
+
+		public static IEnumerable<ISirenEntity> SubEntitiesByRel(
+				this ISirenEntity @this,
+				string rel
+		) {
+			return @this.Entities.Where( entity => entity.Rel.Contains( rel ) );
+		}
+
+		public static IEnumerable<ISirenEntity> SubEntitiesByClass(
+				this ISirenEntity @this,
+				string @class
+		) {
+			return @this.Entities.Where( entity => entity.Class.Contains( @class ) );
+		}
+
+		public static IEnumerable<ISirenEntity> SubEntitiesByType(
+				this ISirenEntity @this,
+				string type
+		) {
+			return @this.Entities.Where( entity => entity.Type.Equals( type ) );
+		}
+
+	}
+
+}
